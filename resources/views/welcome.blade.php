f1104736dbe19895be3f272307f134a44c52d8fc
Prince Sanguan <princesanguan44@gmail.com>
2024-01-26 21:57:41 +0800

    about me fixing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -31,17 +31,14 @@
 
         <section class="about-text">
           <p>
-            I'm Creative Director and UI/UX Designer from Sydney, Australia, working in web development and print media.
-            I enjoy
-            turning complex problems into simple, beautiful and intuitive designs.
+            I am a dedicated Full Stack Developer with a focus on Laravel, weaving 
+            together frontend and backend expertise to create elegant, efficient web 
+            solutions. 
           </p>
 
           <p>
-            My job is to build your website so that it is functional and user-friendly but at the same time attractive.
-            Moreover, I
-            add personal touch to your product and make sure that is eye-catching and easy to use. My aim is to bring
-            across your
-            message and identity in the most creative way. I created web design for many famous brand companies.
+            My passion lies in crafting seamless, user-centric experiences 
+            that push the boundaries of innovation. Let's build the future of the web together.
           </p>
         </section>
 
